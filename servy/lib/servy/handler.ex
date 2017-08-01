30f62cf10943ecf385dea6a35abb5d3d1f6d557a
Raymond Gan <rayning@yahoo.com>
2017-08-01 11:47:17 -0700

    Lesson 7: Make HTTP status and status reason dynamic. Add routes for DELETE and GET /bears/1

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -24,7 +24,11 @@ defmodule Servy.Handler do
 			|> List.first
 			|> String.split(" ")
 			
-		%{method: method, path: path, resp_body: ""}
+		%{ method: method,
+			 path: path,
+			 resp_body: "",
+			 status: nil
+		 }
 	end
 
 	def route(conv) do
@@ -32,22 +36,45 @@ defmodule Servy.Handler do
 	end
 
 	def route(conv, "GET", "/wildthings") do
-		%{ conv | resp_body: "Lions, Tigers, Bears" }
+		%{ conv | status: 200, resp_body: "Lions, Tigers, Bears" }
 	end
 
 	def route(conv, "GET", "/bears") do
-		%{ conv | resp_body: "Teddy, Smokey, Paddington" }
+		%{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+	end
+
+	def route(conv, "GET", "/bears/" <> id) do
+		%{ conv | status: 200, resp_body: "Bear #{id}" }		
+	end
+
+	def route(conv, "DELETE", "/bears/" <> _id) do
+		%{ conv | status: 403, resp_body: "You may not delete a bear" }		
+	end
+
+	def route(conv, _method, path) do
+		%{ conv | status: 404, resp_body: "No #{path} here!"}
 	end
 
 	def format_response(conv) do
 		# Use values in map to make HTTP response string
 		"""
-		HTTP/1.1 200 OK
+		HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
 		Content-Type: text/html
 		Content-Length: 20
 
 		#{conv.resp_body}
 		"""
+	end
+
+	defp status_reason(code) do
+		%{
+			200 => "OK",
+			201 => "Created",
+			401 => "Unauthorized",
+			403 => "Forbidden",
+			404 => "Not Found",
+			500 => "Internal Server Error"
+		}[code]
 	end
 end
 
@@ -63,7 +90,29 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
-GET /bears HTTP/1.1
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
